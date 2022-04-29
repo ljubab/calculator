34bee0e4dfc7ca96a9ae599b121b0afa028f6822
Ljubomir Banovic <ljuba.banovic@gmail.com>
2022-04-29 21:29:51 +0200

    add number typing functionality

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let allButtons = document.querySelectorAll("button");
+let calculatorDisplay = document.querySelector(".calculator-display");
 
-Array.from(allButtons).forEach(btn => {
+let currentNum = "";
+
+// calculatorDisplay.textContent = firstNumber;
+
+let firstNumber = undefined;
+let secondNumber = undefined;
+
+function updateNumbers() {
+    if(secondNumber === undefined) {
+        firstNumber = currentNum;
+    } else {
+        secondNumber = currentNum;
+    }
+}
+
+function numberButtonPressed(btn) {
     let numPattern = "num-";
     let numPatternLocation = btn.id.search(numPattern);
 
@@ -8,6 +24,59 @@ Array.from(allButtons).forEach(btn => {
 
     let numberPressed = +(btn.id.slice(numPattern.length + numPatternLocation));
     btn.addEventListener("click", (e) => {
-        console.log(numberPressed);
+        currentNum += `${numberPressed}`;
+        calculatorDisplay.textContent = currentNum;
+        updateNumbers();
     });
+}
+
+function clearButtonPressed(btn) {
+    let pattern = "clear";
+    let clearPatternLocation = btn.id.search(pattern);
+
+    if(clearPatternLocation === -1) return;
+
+    btn.addEventListener("click", (e) => {
+        currentNum = currentNum.slice(0, currentNum.length - 1);
+        calculatorDisplay.textContent = currentNum;
+        updateNumbers();
+    });
+}
+
+function acButtonPressed(btn) {
+    let pattern = "ac";
+    let acPatternLocation = btn.id.search(pattern);
+
+    if(acPatternLocation === -1) return;
+
+    btn.addEventListener("click", (e) => {
+        firstNumber = secondNumber = undefined;
+        currentNum = "";
+        // calculatorDisplay.textContent = firstNumber;
+        calculatorDisplay.textContent = currentNum;
+        updateNumbers();
+    });
+}
+
+function dotButtonPressed(btn) {
+    let pattern = "dot";
+    let dotPatternLocation = btn.id.search(pattern);
+
+    if(dotPatternLocation === -1) return;
+
+    btn.addEventListener("click", (e) => {
+        let isThereAny = (currentNum.indexOf('.') !== -1);
+        if(isThereAny) return;
+        currentNum += ".";
+        // calculatorDisplay.textContent = firstNumber;
+        calculatorDisplay.textContent = currentNum;
+        updateNumbers();
+    });
+}
+
+Array.from(allButtons).forEach(btn => {
+    numberButtonPressed(btn);
+    clearButtonPressed(btn);
+    acButtonPressed(btn);
+    dotButtonPressed(btn);
 });
